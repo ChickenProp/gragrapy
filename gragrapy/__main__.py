c0a4d099c61db02871fc3986bdbcd3db7b525ddc
Philip Hazelden <philip.hazelden@gmail.com>
2017-05-13 11:18:23 +0100

    Add faceting to __main__.

--- gragrapy/__main__.py
+++ gragrapy/__main__.py
@@ -7,13 +7,20 @@ import argparse
 import gragrapy as gg
 
 def parse_kwargs(kwargs):
-    return { k:v for k,v in [a.split('=', 1) for a in kwargs] }
+    def parse_val(v):
+        try:
+            return int(v)
+        except Exception as e:
+            return v
+
+    return { k: parse_val(v) for k,v in [a.split('=', 1) for a in kwargs] }
 
 def parse_command_line(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument('--data')
     parser.add_argument('--geom', '-g', action='append', nargs='+')
     parser.add_argument('--stat', '-s', action='append', nargs='+')
+    parser.add_argument('--facet', '-f', nargs='+')
     parser.add_argument('aes', nargs='*')
 
     args = parser.parse_args(argv)
@@ -23,6 +30,8 @@ def parse_command_line(argv):
                   for g in args.geom or [] ]
     args.stat = [ getattr(gg.stat, s[0])(**parse_kwargs(s[1:]))
                   for s in args.stat or [] ]
+    if args.facet:
+        args.facet = gg.facet(args.facet[0], **parse_kwargs(args.facet[1:]))
     return args
 
 def main():
@@ -31,7 +40,8 @@ def main():
     data = getattr(gg.data, args.data)
     geoms = args.geom or gg.geom.point
     stats = args.stat or []
-    (gg.Plot(data, args.aes) + geoms + stats).show()
+    facet = args.facet or []
+    (gg.Plot(data, args.aes) + geoms + stats + facet).show()
 
 if __name__ == '__main__':
     main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -7,4 +7,5 @@ from gragrapy.__main__ import parse_kwargs
 def test_parse_kwargs():
     assert parse_kwargs([]) == {}
     assert parse_kwargs(['a=b', 'c=d']) == {'a': 'b', 'c': 'd'}
+    assert parse_kwargs(['a=1', 'c=-5']) == {'a': 1, 'c': -5}
     assert parse_kwargs(['a=b=c', 'c=d']) == {'a': 'b=c', 'c': 'd'}
